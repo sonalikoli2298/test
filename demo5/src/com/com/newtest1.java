cf59e54aa19d67b033a7a162a1d89ffcb65e48ae
A07216trng_M7A.05.16 <A07216trng_M7A.05.16@M7-DX-62C4RG2.dir.svc.accenture.com>
2020-02-25 10:40:28 +0530

    newtest

---
+++ demo5/src/com/com/newtest1.java
@@ -0,0 +1,18 @@
+package com.com;
+
+import org.testng.annotations.Test;
+
+public class newtest1 {
+	@Test
+	  public void f4() {
+		  System.out.println("method 4");
+	  }
+	  @Test
+	  public void f5() {
+		  System.out.println("method 5");
+	  }
+	  @Test
+	  public void f6() {
+		  System.out.println("method 6");
+	  }
+}
